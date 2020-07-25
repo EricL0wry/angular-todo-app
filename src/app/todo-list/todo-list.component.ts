d58a1e752a3a3413a85348a4842786fb01a64265
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-25 11:38:14 -0700

    Sample todo array

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
+  todos = [
+    {
+      id: 1,
+      status: 'new',
+      todo: 'Wash and wax the car',
+    },
+    {
+      id: 2,
+      status: 'new',
+      todo: 'Create a menu',
+    },
+    {
+      id: 3,
+      status: 'new',
+      todo: 'Make a shopping list',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

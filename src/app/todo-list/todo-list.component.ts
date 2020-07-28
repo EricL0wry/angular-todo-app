aaec018724d5e33de559bf18779336df96e675dc
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-28 14:56:13 -0700

    Added delete method

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -19,4 +19,8 @@ export class TodoListComponent implements OnInit {
   getTodos(): void {
     this.todos = this.todoService.getTodos();
   }
+
+  deleteTodo(id: number): void {
+    this.todoService.deleteTodo(id);
+  }
 }

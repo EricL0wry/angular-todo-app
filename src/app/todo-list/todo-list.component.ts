169b1a3726aaca5cc2275469eee80e60cf370117
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-28 10:39:08 -0700

    Refactored to create a getTodos method on the list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -13,6 +13,10 @@ export class TodoListComponent implements OnInit {
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.getTodos;
+  }
+
+  getTodos(): void {
     this.todos = this.todoService.getTodos();
   }
 }
